e1a185b7c01b52d0d4066ae125ad40a21dc36ab3
rjsdnqkr1 <rjsdnqkr1@naver.com>
2022-02-13 18:32:29 +0900

    ➕ Add RIBs dependency

--- Project.swift
+++ Project.swift
@@ -4,10 +4,14 @@ import ProjectDescriptionHelpers
 let project = Project(
     name: "ErrorHandlingRIBs",
     organizationName: "com.gunoooo",
-    packages: [],
+    packages: [
+        .package(url: "https://github.com/uber/RIBs", from: "0.10.0")
+    ],
     targets: Project.appTargets(
         name: "ErrorHandlingRIBs",
-        appDependencies: [],
+        appDependencies: [
+            .ribs
+        ],
         testDependencies: []
     ),
     schemes: [],
---
+++ Tuist/ProjectDescriptionHelpers/Dependency.swift
@@ -0,0 +1,12 @@
+//
+//  Dependency.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by Gunoo on 2022/02/13.
+//
+
+import ProjectDescription
+
+extension TargetDependency {
+    public static let ribs: TargetDependency = .package(product: "RIBs")
+}
